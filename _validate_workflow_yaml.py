36c3b75f6f9ad07222d514380a5a92acfcf4caa9
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2026-03-08 03:56:31 -0400

    remove sha256 from release

--- _validate_workflow_yaml.py
+++ _validate_workflow_yaml.py
@@ -3,5 +3,4 @@ import yaml
 path = Path('/mnt/c/Users/Makin/Desktop/Proton build/proton-arm64-nightlies/.github/workflows/proton-nightly-build.yml')
 obj = yaml.safe_load(path.read_text(encoding='utf-8'))
 print('loaded:', type(obj).__name__)
-for item in obj['jobs']['build']['strategy']['matrix']['include']:
-    print(item['arch_id'], item['profile_version'])
+print('release_steps:', len(obj['jobs']['release']['steps']))
